7251de3558aa8353449acf4c2a3a6ed3be1b791c
Parunchai Timklip <6610685239@cn103>
2026-04-23 20:10:43 +0700

    Add admin.py

--- src/bookings/admin.py
+++ src/bookings/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Room, Booking
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ["username", "first_name", "role", "is_staff"]
+
+    fieldsets = UserAdmin.fieldsets + (("ข้อมูลเพิ่มเติม", {"fields": ("role",)}),)
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Room)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ["room", "user", "purpose_type", "start_time", "status"]
+    list_filter = ["status", "purpose_type", "room"]
