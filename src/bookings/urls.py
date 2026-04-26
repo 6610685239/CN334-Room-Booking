cf1fdfbd9b4c5fe54d5e775574e63f738d506775
Parunchai Timklip <6610685239@cn103>
2026-04-26 22:12:42 +0700

    Add Celendar function

--- src/bookings/urls.py
+++ src/bookings/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.tu_login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("book/", views.create_booking_view, name="book_room"),
+    path("calendar/", views.calendar_view, name="calendar"),
+    path("api/booked-slots/", views.api_get_booked_slots, name="api_booked_slots"),
 ]
